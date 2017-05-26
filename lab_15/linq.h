71ccf85ed3263da15bf7ddc69e2b7411c664d3c8
AndreySmirdin <andrey.smirdin@mail.ru>
2017-05-27 02:13:12 +0300

    more keywords

--- lab_15/linq.h
+++ lab_15/linq.h
@@ -22,12 +22,12 @@ class where_enumerator;
 template<typename T>
 class enumerator {
 public:
-    virtual T operator*() = 0; // Получает текущий элемент.
-    virtual enumerator& operator++() = 0;  // Переход к следующему элементу
-    virtual operator bool() const = 0;  // Возвращает true, если есть текущий элемент
+    virtual const T& operator*() const = 0;
+    virtual enumerator& operator++() = 0;
+    virtual operator bool() const = 0;
 
     auto drop(int count) {
-        return drop_enumerator<T>(*this, count);
+        return drop_enumerator<T>(*this, count); // I dont like this class. Function will be better.
     }
 
     auto take(int count) {
@@ -36,7 +36,7 @@ public:
 
     template<typename U = T, typename F>
     auto select(F func) {
-        return select_enumerator<U, T, F>(*this, func);
+        return select_enumerator<U, T, F>(*this, func); // Order of template arguments is important.
     }
 
     template<typename F>
@@ -44,8 +44,8 @@ public:
         return until_enumerator<T, F>(*this, func);
     }
 
-    auto until_eq(T v){
-        return until([v](T x){ return x == v; });
+    auto until_eq(const T& v){
+        return until([&v](const T& x){ return x == v; });
     }
 
     std::vector<T> to_vector() {
@@ -71,8 +71,8 @@ public:
         return where_enumerator<T, F>(*this, func);
     }
 
-    auto where_neq(T v){
-        return where([v](T x) { return x != v; });
+    auto where_neq(const T& v){
+        return where([&v](const T& x) { return x != v; });
     }
 };
 
@@ -82,16 +82,16 @@ public:
     range_enumerator(Iter begin, Iter end) : begin_(begin), end_(end) {
     }
 
-    T operator*() {
+    const T& operator*() const override {
         return *begin_;
     }
 
-    range_enumerator& operator++() {
+    range_enumerator& operator++() override {
         begin_++;
         return *this;
     }
 
-    operator bool() const {
+    operator bool() const override {
         return begin_ != end_;
     }
 private:
@@ -113,14 +113,14 @@ public:
             ++parent_;
     }
 
-    T operator*(){ return *parent_; }
+    const T& operator*() const override { return *parent_; }
 
-    drop_enumerator& operator++() {
+    drop_enumerator& operator++() override {
         ++parent_;
         return *this;
     }
 
-    operator bool() const { return parent_; }
+    operator bool() const override { return parent_; }
 
 private:
     enumerator<T>& parent_;
@@ -130,67 +130,69 @@ private:
 template<typename T>
 class take_enumerator : public enumerator<T> {
 public:
-    take_enumerator(enumerator<T> &parent, int count) : parent_(parent), count_(count) { taken_ = 0;}
+    take_enumerator(enumerator<T> &parent, int count) : parent_(parent), count_(count) {}
 
-    T operator*()  { return *parent_; }
+    const T& operator*() const override { return *parent_; }
 
-    take_enumerator& operator++() {
+    take_enumerator& operator++() override {
         ++parent_;
-        ++taken_;
+        --count_;
         return *this;
     }
 
-    operator bool() const {
-        return taken_ < count_ && parent_;
+    operator bool() const override {
+        return count_ > 0 && parent_;
     }
 
 private:
     enumerator<T>& parent_;
-    int taken_, count_;
+    int count_;
 };
 
 
 template<typename T, typename U, typename F>
 class select_enumerator : public enumerator<T> {
 public:
-    select_enumerator(enumerator<U> &parent, F func) : parent_(parent), func_(func) {
+    select_enumerator(enumerator<U> &parent, F func) : parent_(parent), func_(std::move(func)) {
     }
 
-    T operator*()  {
-        return func_(*parent_);
+    const T& operator*() const override {
+        current_ = func_(*parent_);
+        return current_;
     }
 
-    select_enumerator& operator++() {
+    select_enumerator& operator++() override {
         ++parent_;
         return *this;
     }
 
-    operator bool() const { return parent_; }
+    operator bool() const override { return parent_; }
 
 private:
     enumerator<U>& parent_;
-    F func_;
+    mutable F func_; // Need mutable because we want operator* to be const, but func_ can change.
+    mutable T current_;
 };
 
 
 template<typename T, typename F>
 class until_enumerator : public enumerator<T> {
 public:
-    until_enumerator(enumerator<T> &parent, F predicate) : parent_(parent), predicate_(predicate) {}
+    until_enumerator(enumerator<T> &parent, F predicate) : parent_(parent), predicate_((std::move(predicate))) {}
 
-    T operator*()  { return *parent_; }
+    const T& operator*() const override { return *parent_; }
 
-    until_enumerator& operator++() {
+    until_enumerator& operator++() override  {
         ++parent_;
         return *this;
     }
 
-    operator bool() const {
+    operator bool() const override {
         return parent_ && !predicate_(*parent_);
     }
 private:
     enumerator<T>& parent_;
-    F predicate_;
+    mutable F predicate_;
 };
 
 
@@ -202,16 +204,16 @@ public:
             this->operator++();
     }
 
-    T operator*()  { return *parent_; }
+    const T& operator*() const override { return *parent_; }
 
-    where_enumerator& operator++() {
+    where_enumerator& operator++() override  {
         ++parent_;
         while (parent_ && !predicate_(*parent_))
             ++parent_;
         return *this;
     }
 
-    operator bool() const { return parent_; }
+    operator bool() const override { return parent_; }
 private:
     enumerator<T>& parent_;
     F predicate_;
